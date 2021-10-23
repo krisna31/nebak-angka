f42652c08ca2a74d401a85a467b58b20511a5d88
Krisna <krisnaaaputraaa@gmail.com>
2021-10-23 11:21:53 +0700

    refactoring the comp choices code

--- nebak-angka.js
+++ nebak-angka.js
@@ -1,29 +1,29 @@
 var tanya = true
 while (tanya) {
   var nyawa = 3
-
-  var comp = Math.random();
-  if (comp >= 0 && comp < 0.1) {
-    comp = 1;
-  } else if (comp >= 0.1 && comp < 0.2) {
-    comp = 2;
-  } else if (comp >= 0.2 && comp < 0.3) {
-    comp = 3;
-  } else if (comp >= 0.3 && comp < 0.4) {
-    comp = 4;
-  } else if (comp >= 0.4 && comp < 0.5) {
-    comp = 5;
-  } else if (comp >= 0.5 && comp < 0.6) {
-    comp = 6;
-  } else if (comp >= 0.6 && comp < 0.7) {
-    comp = 7;
-  } else if (comp >= 0.7 && comp < 0.8) {
-    comp = 8;
-  } else if (comp >= 0.8 && comp < 0.9) {
-    comp = 9;
-  } else {
-    comp = 10;
-  }
+  var comp = Math.round(Math.random() * 10);
+  // var comp = Math.random();
+  // if (comp >= 0 && comp < 0.1) {
+  //   comp = 1;
+  // } else if (comp >= 0.1 && comp < 0.2) {
+  //   comp = 2;
+  // } else if (comp >= 0.2 && comp < 0.3) {
+  //   comp = 3;
+  // } else if (comp >= 0.3 && comp < 0.4) {
+  //   comp = 4;
+  // } else if (comp >= 0.4 && comp < 0.5) {
+  //   comp = 5;
+  // } else if (comp >= 0.5 && comp < 0.6) {
+  //   comp = 6;
+  // } else if (comp >= 0.6 && comp < 0.7) {
+  //   comp = 7;
+  // } else if (comp >= 0.7 && comp < 0.8) {
+  //   comp = 8;
+  // } else if (comp >= 0.8 && comp < 0.9) {
+  //   comp = 9;
+  // } else {
+  //   comp = 10;
+  // }
   // rules
   for (nyawa; nyawa > 0;) {
     var user = prompt('masukkan angka antara 1-10\nNyawa tersisa ' + nyawa);
